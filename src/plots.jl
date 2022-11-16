df237f2339517473a3ed1fd3396db05ac0dac927
leif <leif@denby.eu>
2022-11-16 14:39:54 +0000

    add legend to plots

--- src/plots.jl
+++ src/plots.jl
@@ -1,14 +1,20 @@
 
 function plot_profile(sols...; vars=[:r, :w, :T, :qv, :ql, :rh, :Δrho], wrap=3, size=400)
-    plot_parts = []
-    for var_name in vars
-        push!(plot_parts, plot_profile_var(sols..., var_name=var_name))
-    end
-    
-    ncols = minimum([wrap, length(plot_parts)])
-    nrows = length(plot_parts) ÷ wrap
-    if ncols * nrows < length(plot_parts)
+    N = length(vars)
+    ncols = minimum([wrap, N])
+    nrows = N ÷ wrap
+    if ncols * nrows < N
         nrows += 1
+    end
+
+    plot_parts = []
+    for (i, var_name) in enumerate(vars)
+        if i == N
+            add_legend = true
+        else
+            add_legend = false
+        end
+        push!(plot_parts, plot_profile_var(sols..., var_name=var_name, legend=add_legend))
     end
 
     plot(
@@ -19,8 +25,14 @@ function plot_profile(sols...; vars=[:r, :w, :T, :qv, :ql, :rh, :Δrho], wrap=3,
     )
 end
 
+function make_name(sol)
+    params = Dict(pairs(sol.prob.p))
+    pop!(params, :environment)
+    params_s = join(["$(k)=$(v)" for (k,v) in params], " ")
+    return params_s
+end
 
-function plot_profile_var(sols...; var_name=:r)
+function plot_profile_var(sols...; var_name=:r, legend=:outertopright)
     env_linestyle = :dash
 
     desc = Dict(
@@ -31,14 +43,15 @@ function plot_profile_var(sols...; var_name=:r)
         :w => "vertical velocity [m/s]"
     )
     
-    p_var = plot()
+    p_var = plot(;legend=legend)
     
     g(sol, v) = getindex.(sol.u, v)
     sg(sol, v, s) = g(sol, v) .* s
 
     colors = palette(:tab10)
     for (sol, color) in zip(sols, colors)
-        pg(sol, v) = plot!(p_var, g(sol, v), g(sol, :z), xlabel=desc[v], label="", ylabel="alt [m]", color=color)
+        label = make_name(sol)
+        pg(sol, v) = plot!(p_var, g(sol, v), g(sol, :z), xlabel=desc[v], label=label, ylabel="alt [m]", color=color)
         params = sol.prob.p
 
         z_prof = g(sol, :z)
@@ -64,20 +77,20 @@ function plot_profile_var(sols...; var_name=:r)
             pg(sol, :T)
             plot!(p_var, ustrip.(T_env), ustrip.(z_prof), label=nothing, linestyle=env_linestyle, color=color)
         elseif var_name == :qv
-            p = plot!(p_var, sg(sol, :q_v, 1.0e3), z_prof, label=nothing, xlabel="water vapour conc [g/kg]", color=color)
-            plot!(p_var, qv_env .* 1.0e3, z_prof, label=nothing, color=color, linestyle=env_linestyle)
-            plot!(p_var, qv_sat_cld .* 1.0e3, z_prof, label=nothing, color=color, linestyle=:dashdot)
+            p = plot!(p_var, sg(sol, :q_v, 1.0e3), z_prof, label=label, xlabel="water vapour conc [g/kg]", color=color)
+            plot!(p_var, qv_env .* 1.0e3, z_prof, label=label, color=color, linestyle=env_linestyle)
+            plot!(p_var, qv_sat_cld .* 1.0e3, z_prof, label=label, color=color, linestyle=:dashdot)
         elseif var_name == :rh
-            plot!(p_var, rh_c, g(sol, :z), label="", xlabel="relative humidity [1]", color=color)
+            plot!(p_var, rh_c, g(sol, :z), label=label, xlabel="relative humidity [1]", color=color)
             if length(p_var.series_list) == 1
-                vline!(p_var, [1.0], linestyle=:dash, color=:black, label=nothing)
+                vline!(p_var, [1.0], linestyle=:dash, color=:black, label=label)
             end
             plot!(p_var, rh_env, g(sol, :z), label="", color=color, linestyle=env_linestyle)
         elseif var_name == :Δrho
             Δrho = ustrip.(ρ_cld - ρ_env) .* 1.0e3
-            plot!(p_var, Δrho, z_prof, xlabel="Δρ [g/m^3]", color=color, label=nothing)
+            plot!(p_var, Δrho, z_prof, xlabel="Δρ [g/m^3]", color=color, label=label)
         elseif var_name == :ql
-            plot!(p_var, sg(sol, :q_l, 1.0e3), z_prof, xlabel=desc[:q_l], label="", color=color)
+            plot!(p_var, sg(sol, :q_l, 1.0e3), z_prof, xlabel=desc[:q_l], label=label, color=color)
         else
             throw("plotting of $(var_name) not implemented")
         end
