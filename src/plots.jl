6273d17015da15b455f841dfe84857d001169884
leif <leif@denby.eu>
2022-11-16 14:09:14 +0000

    more tweaks to plots

--- src/plots.jl
+++ src/plots.jl
@@ -45,11 +45,12 @@ function plot_profile_var(sols...; var_name=:r)
         p_env = uconvert.(u"Pa", params.environment.(z_prof * u"m", :p))
         T_env = uconvert.(u"K", params.environment.(z_prof * u"m", :T))
         qv_env = params.environment.(z_prof * u"m", :qv)
-        T_cld = g(sol, :T) * u"K"
-        qv_sat_env = CloudModels.calc_qv_sat.(T_env, p_env)
-        qv_sat_cld = CloudModels.calc_qv_sat.(T_cld, p_env)
-        rh_prof = g(sol, :q_v) ./ qv_sat_cld
         ρ_env = params.environment.(z_prof * u"m", :rho)
+        rh_env = params.environment.(z_prof * u"m", :rh)
+
+        T_cld = g(sol, :T) * u"K"
+        qv_sat_cld = CloudModels.calc_qv_sat.(T_cld, p_env)
+        rh_c = g(sol, :q_v) ./ qv_sat_cld
 
         qv_cld, ql_cld, qr_cld, qi_cld = g(sol, :q_v), g(sol, :q_l), g(sol, :q_r), g(sol, :q_i)
         qd_cld = 1.0 .- (qv_cld- ql_cld - qr_cld - qi_cld)
@@ -64,16 +65,17 @@ function plot_profile_var(sols...; var_name=:r)
             plot!(p_var, ustrip.(T_env), ustrip.(z_prof), label=nothing, linestyle=env_linestyle, color=color)
         elseif var_name == :qv
             p = plot!(p_var, sg(sol, :q_v, 1.0e3), z_prof, label=nothing, xlabel="water vapour conc [g/kg]", color=color)
-            plot!(p_var, qv_env .* 1.0e3, z_prof, label=nothing, color=color, linestyle=:dashdot)
+            plot!(p_var, qv_env .* 1.0e3, z_prof, label=nothing, color=color, linestyle=env_linestyle)
             plot!(p_var, qv_sat_cld .* 1.0e3, z_prof, label=nothing, color=color, linestyle=:dashdot)
         elseif var_name == :rh
-            plot!(p_var, rh_prof, g(sol, :z), label="", xlabel="relative humidity [1]", color=color)
+            plot!(p_var, rh_c, g(sol, :z), label="", xlabel="relative humidity [1]", color=color)
             if length(p_var.series_list) == 1
                 vline!(p_var, [1.0], linestyle=:dash, color=:black, label=nothing)
             end
+            plot!(p_var, rh_env, g(sol, :z), label="", color=color, linestyle=env_linestyle)
         elseif var_name == :Δrho
             Δrho = ustrip.(ρ_cld - ρ_env) .* 1.0e3
-            plot!(p_var, Δrho, z_prof, xlabel="Δρ [g/m^3]", color=color)
+            plot!(p_var, Δrho, z_prof, xlabel="Δρ [g/m^3]", color=color, label=nothing)
         elseif var_name == :ql
             plot!(p_var, sg(sol, :q_l, 1.0e3), z_prof, xlabel=desc[:q_l], label="", color=color)
         else
